80ee278b3211ac7f8afd37e867d658af47608743
marcan_m <marcandre.moreau@gmail.com>
2010-01-11 17:26:25 -0500

    Initializing repository

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Team 7</title>
     </head>
     <body>
         <?php
