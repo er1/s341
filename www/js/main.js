410b3bf93a7e76ce46bbcb9766a5df6675b812e0
Mathieu Dumais-savard <matdumsa@gmail.com>
2010-01-24 14:59:13 -0500

    Documented requirements for a calendar control in UI in requirement/UI requirement.doc. Found a control that match my requirement list. Implemented a prototype in UI about the calendar. see the website for more details. Described the interface for moving calendar data around 1tier and 2ndtier in www/json/myschedule.json

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,19 @@
 $(function() {	//To be run when DOM is constructed
 	$("#tabs").tabs({fx: { opacity: 'toggle' } });
+
+	  $('#calViewSchedule').weekCalendar({
+		readonly:false
+		,date:new Date(2010, 00,5)
+		,businessHours: {start:8, end:22, limitDisplay:true}
+		,timeslotsPerHour: 4
+	    ,data:"json/myschedule.json"
+		,height: function($calendar){
+			return $(window).height()-200;
+		}
+		,eventDrop:function(calEvent, element)
+		{
+			alert('Obviously you should not drag&drop courses you are in but its just a demo');
+		
+		}	
+	});
 });
