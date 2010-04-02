fbf0058317effc2f1350692812c3a67bb3592f47
C-o-r-E <can.of.tuna@gmail.com>
2010-04-02 00:52:18 -0400

    quick way to clear the db added

---
+++ scripts/RESETDB.sql
@@ -0,0 +1,9 @@
+delete from SequenceDetails;
+delete from Program where not Name='DERP';
+delete from Satisfies;
+delete from Requires;
+delete from ClassBlock;
+delete from Class;
+delete from Course where not DepartmentID='DERP';
+delete from Department where not DepartmentID='DERP';
+delete from Faculty where not FacultyID='DERP';
