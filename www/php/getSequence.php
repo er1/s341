e7eb23e16faad0d20ba8bde1e93ba26f120841e5
C-o-r-E <can.of.tuna@gmail.com>
2010-04-06 14:01:50 -0400

    just poking some stuff initially

---
+++ www/php/getSequence.php
@@ -0,0 +1,16 @@
+<?php
+require_once("Main.php");
+require_once("Authentication.php");
+
+$auth->EnforceCurrentLevel( 2 );
+
+$q = 'select * from SequenceDetails where ProgramID=(select ProgramID from Program where DepartmentID=\'SOEN\');'
+result = $db->Query( $q );
+
+echo("CourseID\t Semester\n");
+while($row = mysql_fetch_row( $result ) )
+{
+	echo("" . $row[1] . "\t" . $row[2] . "\n");
+}
+
+?>
