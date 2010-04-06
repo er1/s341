00840e838bd389c6cca3cede1fabdf986a463fca
C-o-r-E <can.of.tuna@gmail.com>
2010-04-06 15:04:13 -0400

    minor changes

--- www/php/getSequence.php
+++ www/php/getSequence.php
@@ -10,7 +10,13 @@ $result = $db->Query( $q );
 echo("CourseID\t Semester\n");
 while($row = mysql_fetch_row( $result ) )
 {
-	echo("" . $row[1] . "\t" . $row[2] . "\n");
+	$q2 = 'select DepartmentID, Number from Course where CourseID=\'' .
+		$row[1] . '\';';
+	$subResult = $db->Query( $q2 );
+	$subRow = mysql_fetch_row( $subResult ) ) or dieNicely("Expected Course info for ID#" . $row[1]);
+	echo("" . $subRow[0] . $subRow[1] . "\t" . $row[2] . "\n");
+	
+	//echo("" . $row[1] . "\t" . $row[2] . "\n");
 }
 
 ?>
